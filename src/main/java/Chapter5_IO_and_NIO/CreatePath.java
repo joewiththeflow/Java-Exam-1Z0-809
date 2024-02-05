abafb5e0ebeb29136dfeccd4beda9219e201f7b6
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2024-02-05 08:22:59 +0000

    File and Directory creation

---
+++ src/main/java/Chapter5_IO_and_NIO/CreatePath.java
@@ -0,0 +1,43 @@
+package Chapter5_IO_and_NIO;
+
+import java.io.File;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CreatePath {
+    public static void main(String[] args) {
+        Path p1 = Paths.get("/tmp/file.txt");   // UNIX
+        Path p2 = Paths.get("c:\\temp\\test");  // Windows
+
+        Path p3 = Paths.get("/tmp", "file1.txt");       // same as p1
+        Path p4 = Paths.get("c:", "temp", "test");      // same as p2
+        Path p5 = Paths.get("c:\\temp", "test");        // same as p2
+
+        // You need to be careful if your path is not starting at root
+        Path p6 = Paths.get("tmp", "file1.txt");    // RELATIVE PATH - not the same as p1
+            // If the program is run from root, this would be: /tmp/file1.txt
+            // If the program is run from /tmp, this would be: /tmp/tmp/file1.txt
+
+
+        // Sometimes you might have a path like this "file:///c:/tmp"
+        // But the following will complain about the colon after file and is not allowed:
+        Path p7  = Paths.get("file:///c:/tmp/test");
+
+        // To solve the problem you can use:
+        Path p8 = Paths.get(URI.create("file:///c:/tmp"));
+
+
+        // Paths.get() is actually a shortcut as there is a check to find out which filesystem type is in use
+        Path shorter = Paths.get("c:", "tmp");
+        Path longer = FileSystems.getDefault().getPath("c:", "temp");
+
+
+        // If you want to convert between code that is pre Java 7 with later versions
+        File file = new File("pretendylocation");
+        Path convertedToPath = file.toPath();
+        File convertedToFile = convertedToPath.toFile();
+    }
+}
---
+++ src/main/java/Chapter5_IO_and_NIO/FilesAndDirsWithPath.java
@@ -0,0 +1,49 @@
+package Chapter5_IO_and_NIO;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FilesAndDirsWithPath {
+    public static void main(String[] args) {
+
+        Path path = Paths.get("fileWrite5.txt");
+        System.out.println(Files.exists(path));
+
+        try {
+            Files.createFile(path);         // IOException if File exists
+        } catch (IOException e) { }
+
+        System.out.println(Files.exists(path));
+
+
+        // Create Directories - The LONG WAY
+        Path path0 = Paths.get("java");
+        Path path1 = Paths.get("java/source");
+        Path path2 = Paths.get("java/source/directory");
+        Path file = Paths.get("java/source/directory/Program.java");
+
+        try {
+            Files.createDirectory(path0);           // Create dir 0
+            Files.createDirectory(path1);           // Create dir 1
+            Files.createDirectory(path2);          // Create dir 2
+            Files.createFile(file);                 // Create file
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        // Create Directories in one go
+        Path path3 = Paths.get("java2/source/directory");
+        Path file2 = Paths.get("java2/source/directory/Program.java");
+        try {
+            Files.createDirectories(path3);         // Create all dirs
+            Files.createFile(file2);                 // Create file
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Remember that when a file or directory already exists, an IOException is thrown
+    }
+}
