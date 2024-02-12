a67ed3c08b4773d099856279d4a4e4d27bbb908c
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2024-02-12 19:58:53 +0000

    Serialize and Deserialize a Cat object

--- src/main/java/Chapter5_IO_and_NIO/Serialization/Cat.java
+++ src/main/java/Chapter5_IO_and_NIO/Serialization/Cat.java
@@ -8,20 +8,20 @@ public class Cat implements Serializable {      // has to implement Serializable
     private String name = "Mittens";
     public static void main(String[] args) {
 
-        Cat c = new Cat();
-        Cat d;
+        Cat c = new Cat();                      //declare cat to be serialized
+        Cat d;                                  // cat will be read into d
 
         try {
             FileOutputStream fos = new FileOutputStream("src/main/java/Chapter5_IO_and_NIO/Serialization/catSerialized.txt");
             ObjectOutputStream os = new ObjectOutputStream(fos);
-            os.writeObject(c);
+            os.writeObject(c);                                      // serialize and write the object to file
             os.close();
         } catch (IOException e) {}
 
         try {
             FileInputStream fis = new FileInputStream("src/main/java/Chapter5_IO_and_NIO/Serialization/catSerialized.txt");
             ObjectInputStream ois = new ObjectInputStream(fis);
-            d = (Cat) ois.readObject();
+            d = (Cat) ois.readObject();                             // deserialize and read the object
             System.out.println("Cat's name is " + d.name);
             ois.close();
         } catch (Exception e) {}
