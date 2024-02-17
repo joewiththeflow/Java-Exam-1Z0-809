b98f21e6dd95018c551cf82e8aaa850db3dbc58f
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2024-02-17 11:52:45 +0000

    Simple example of override of equals method

--- src/main/java/Chapter6_Generics_and_Collections/Hash_Codes_Equals_toString/EqualsTest.java
+++ src/main/java/Chapter6_Generics_and_Collections/Hash_Codes_Equals_toString/EqualsTest.java
@@ -1,7 +1,10 @@
-package Chapter6_Generics_and_Collections;
+package Chapter6_Generics_and_Collections.Hash_Codes_Equals_toString;
 
 public class EqualsTest {
     public static void main(String[] args) {
+        System.out.println("x".equals(null));           // ---> just put this extra line in here to show that
+                                                        // the equals method for an object should return false
+                                                        // if the reference value is compared with null
         Moof one = new Moof(8);
         Moof two = new Moof(8);
         if (one.equals(two)) {
