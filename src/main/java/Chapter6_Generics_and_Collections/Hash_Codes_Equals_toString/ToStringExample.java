8d818a7c2e545bd20941d47582c4533700ec9878
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2024-02-17 11:52:11 +0000

    Simple toString() example

---
+++ src/main/java/Chapter6_Generics_and_Collections/Hash_Codes_Equals_toString/ToStringExample.java
@@ -0,0 +1,20 @@
+package Chapter6_Generics_and_Collections.Hash_Codes_Equals_toString;
+
+public class ToStringExample {
+    public static void main(String[] args) {
+        Bob f = new Bob("GoBobGo", 19);
+        System.out.println(f);
+    }
+}
+
+class Bob {
+    int shoeSize;
+    String nickName;
+    Bob(String nickName, int shoeSize) {
+        this.shoeSize = shoeSize;
+        this.nickName = nickName;
+    }
+    public String toString() {
+        return ("I am a Bob, but you can call me " + nickName + ". My shoe size is " + shoeSize);
+    }
+}
