b6969c91333de1e6823d06be2389c25b7b603bcb
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2024-02-18 09:52:35 +0000

    Uninitialised Static Integer is null - be careful

--- src/main/java/Chapter6_Generics_and_Collections/Using_Collections/BoxingWatchForNull.java
+++ src/main/java/Chapter6_Generics_and_Collections/Using_Collections/BoxingWatchForNull.java
@@ -1,17 +1,19 @@
-package Chapter6_Generics_and_Collections;
+package Chapter6_Generics_and_Collections.Using_Collections;
 
 import java.sql.SQLOutput;
 
 public class BoxingWatchForNull {
     static Integer x;
+    static int y;
 
     public static void main(String[] args) {
-        doStuff(x);
+        doStuff(y);     // this works as int default to 0
+        doStuff(x);     // will pass null
     }
 
     private static void doStuff(int z) {
         int z2 = 5;
-        System.out.println(z2 + z);
+        System.out.println(z2 + z);         // NullPointerException when z is null (i.e. when x was passed into the function)
     }
 }
 
