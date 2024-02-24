d243ee564b1ee2e7c60972c66458118fd8b5151b
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2024-02-24 15:24:09 +0000

    @FunctionalInterface annotation added

--- src/main/java/Chapter8_Lambda_Expressions/Syntax/DogQuerier.java
+++ src/main/java/Chapter8_Lambda_Expressions/Syntax/DogQuerier.java
@@ -1,5 +1,6 @@
 package Chapter8_Lambda_Expressions.Syntax;
 
+@FunctionalInterface                    // This annotation is not required but allows compiler to war if you add another method
 public interface DogQuerier {
     public boolean test(Dog d);
 }
