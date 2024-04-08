5ab591dca2c59c348f5392b456b682286d28285b
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2024-04-08 22:27:33 +0100

    Question 1

---
+++ src/main/java/Chapter4_Dates_Times_Locales/Questions/Question1.java
@@ -0,0 +1,32 @@
+package Chapter4_Dates_Times_Locales.Questions;
+
+import java.time.ZonedDateTime;
+
+public class Question1 {
+    public static void main(String[] args) {
+        // Given the code fragment
+        ZonedDateTime zd = ZonedDateTime.parse("2020-05-04T08:05:00");
+        System.out.println(zd.getMonth() + " " + zd.getDayOfMonth());
+    }
+
+//    A. MAY 4
+//    B. APRIL 5
+//    C. MAY 4 2020
+//    D. APRIL 5 2020
+//    E. Compilation fails
+//    F. Runtime Exception      --> CORRECT, no zone id in string
+
+}
+
+// 1. E ---- should be F
+// 2. A, B, E
+// 3. D ---- should be E
+// 4. B ---- should also be C
+// 5. D ---- should be C
+// 6. A, C, D, E
+// 7. D
+// 8.
+// 9. C ---- should also be E
+// 10. D
+// 11. E ---- should be C
+// 12. E ---- should be A
